c2d62451d191f64052c71552821f78a5fc6bd8e5
rkouere <rkouere@gmail.com>
2015-03-03 19:36:45 +0100

    Get working directory and store it.

--- car-tp2/src/main/java/car/PasserelleRest.java
+++ car-tp2/src/main/java/car/PasserelleRest.java
@@ -7,6 +7,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 /**
@@ -21,6 +22,7 @@ public class PasserelleRest {
         FTPClient ftp = new FTPClient();
         FTPClientConfig config = new FTPClientConfig();
         int reply;
+        String currentDirectory = new String();
         
         /**
          * Initialise une connection client avec le ftp.
@@ -29,23 +31,32 @@ public class PasserelleRest {
         public PasserelleRest() throws IOException {
             ftp.configure(config );
             ftp.connect("127.0.0.1", 4000);
-            System.out.println("[passerelle]: connected to ftp server.");
-            System.out.print(ftp.getReplyString());
+            System.out.println("[passerelle]: connection to ftp server.");
+            System.out.print("[passerelle]: " + ftp.getReplyString());
             ftp.login("nico", "password");
-            System.out.print(ftp.getReplyString());
+            System.out.print("[passerelle]: " + ftp.getReplyString());
             reply = ftp.getReplyCode();
+            /* we set the current directory */
+            this.currentDirectory = ftp.printWorkingDirectory();
+            /* if we can't connect to the ftp */
             if(!FTPReply.isPositiveCompletion(reply)) {
                 ftp.disconnect();
                 System.err.println("FTP server refused connection.");
                 System.exit(1);
             }
-            
         }
         
 	@GET
 	@Produces("text/html")
-	public String sayHello() {
-		return "<h1>Hello World</h1>";
+	public String sayHello() throws IOException {
+            String res = new String();
+            FTPFile[] files = ftp.listFiles(this.currentDirectory);
+            for (FTPFile file : files) {
+                res += file;
+            }
+            return "[passerelle]: " + res;
+
+		//return "<h1>Hello World</h1>";
 	}
 
 	 @GET
